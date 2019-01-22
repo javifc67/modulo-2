6f669a2b3a6321c49344f7f10fc6875c79914508
Javier Ferreño <javifc67@gmail.com>
2019-01-22 18:05:11 +0100

    dia 1

---
+++ dia-1/app.js
@@ -0,0 +1,21 @@
+/*
+ * Aquí podéis hacer los ejercicios y
+ * practicar Javascript!
+ */
+
+console.log("Probando, probando, 1, 2, 3!");
+
+let nombre;
+nombre = "javier";
+let apellido = "Ferre";
+
+//alert("hola" + " mi nombre es " + nombre + " " + apellido);
+
+let admin;
+let name;
+name = "john";
+admin = name;
+alert(admin);
+
+let OurplanetName = "Earth";
+let visitor;
