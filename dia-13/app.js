aa125a5e682cbe5715f1d85e4207467336e8b11b
Javier Ferreño <javifc67@gmail.com>
2019-02-06 18:37:42 +0100

    Fin del modulo 2

--- dia-13/app.js
+++ dia-13/app.js
@@ -9,7 +9,7 @@ let user2 = JSON.parse(json);
 alert(user2);
  */
 
-let room = {
+/* let room = {
   number: 23
 };
 
@@ -31,3 +31,66 @@ alert(
     }
   })
 );
+ */
+
+// ASINCRONIIA
+
+/* function loadScript(src, callback) {
+  let script = document.createElement("script");
+  script.src = src;
+  script.onload = () => callback(script);
+  document.head.append(script);
+}
+
+loadScript(
+  "https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.js",
+  script => {
+    alert(`Cool, the ${script.src} is loaded`);
+    alert(_); // function declared in the loaded script
+    console.log(_.sum([4, 2, 8, 6]));
+
+  }
+);
+ */
+
+/* function loadScript(src, callback) {
+  let script = document.createElement("script");
+  script.src = src;
+
+  script.onload = () => callback(null, script);
+  script.onerror = () => callback(new Error(`Script load error ` + src));
+
+  document.head.append(script);
+}
+
+function loadScript(src) {
+  return new Promise(function(resolve, reject) {
+    let script = document.createElement("script");
+    script.src = src;
+
+    script.onload = () => resolve(script);
+    script.onerror = () => reject(new Error("Script load error: " + src));
+
+    document.head.append(script);
+  });
+}
+
+let promise = loadScript(
+  "https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js"
+);
+
+promise.then(
+  script => alert(`${script.src} is loaded!`),
+  error => alert(`Error: ${error.message}`)
+);
+
+promise.then(script => alert("One more handler to do something else!"));
+ */
+/* let promise = new Promise(function(resolve, reject) {
+  resolve(1);
+
+  setTimeout(() => resolve(2), 1000);
+});
+
+promise.then(alert);
+ */
